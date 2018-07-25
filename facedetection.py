e780eb8eb65db357d8915cd82a38f84b43e906e6
iitianavinash3142 <iitianavinash123@gmail.com>
2018-07-26 01:44:21 +0530

    first commit

---
+++ facedetection.py
@@ -0,0 +1,114 @@
+#Python 2.7.2
+#Opencv 2.4.2
+#PIL 1.1.7
+
+import cv #Opencv
+import cv2
+from PIL import Image #Image from PIL
+import glob
+import os, sys
+
+
+def DetectFace(image, faceCascade, returnImage=False):
+    # This function takes a grey scale cv image and finds
+    # the patterns defined in the haarcascade function
+
+    #variables    
+    min_size = (20,20)
+    haar_scale = 1.1
+    min_neighbors = 3
+    haar_flags = 0
+
+    # Equalize the histogram
+    cv.EqualizeHist(image, image)
+
+    print(faceCascade)
+
+    # Detect the faces
+    faces = cv.HaarDetectObjects(
+       image, faceCascade, cv.CreateMemStorage(0),
+       haar_scale, min_neighbors, haar_flags, min_size
+       )
+
+    # If faces are found
+    if faces and returnImage:
+        for ((x, y, w, h), n) in faces:
+            # Convert bounding box to two CvPoints
+            pt1 = (int(x), int(y))
+            pt2 = (int(x + w), int(y + h))
+            cv.Rectangle(image, pt1, pt2, cv.RGB(255, 0, 0), 5, 8, 0)
+
+    if returnImage:
+        return image
+    else:
+        return faces
+
+def pil2cvGrey(pil_im):
+    # Convert a PIL image to a greyscale cv image
+
+    pil_im = pil_im.convert('L')
+    cv_im = cv.CreateImageHeader(pil_im.size, cv.IPL_DEPTH_8U, 1)
+    cv.SetData(cv_im, pil_im.tobytes(), pil_im.size[0]  )
+    return cv_im
+
+def cv2pil(cv_im):
+    # Convert the cv image to a PIL image
+    return Image.frombytes("L", cv.GetSize(cv_im), cv_im.tostring())
+
+def imgCrop(image, cropBox, boxScale=1):
+    # Crop a PIL image with the provided box [x(left), y(upper), w(width), h(height)]
+
+    # Calculate scale factors
+    xDelta=max(cropBox[2]*(boxScale-1),0)
+    yDelta=max(cropBox[3]*(boxScale-1),0)
+
+    # Convert cv box to PIL box [left, upper, right, lower]
+    PIL_box=[cropBox[0]-xDelta, cropBox[1]-yDelta, cropBox[0]+cropBox[2]+xDelta, cropBox[1]+cropBox[3]+yDelta]
+
+    return image.crop(PIL_box)
+
+def faceCrop(imagePattern,boxScale=1):
+    # Select one of the haarcascade  according to crop files:
+    #   haarcascade_frontalface_alt.xml  <-- we use this
+    #   haarcascade_frontalface_alt2.xml
+    #   haarcascade_frontalface_alt_tree.xml
+    #   haarcascade_frontalface_default.xml
+    #   haarcascade_profileface.xml
+    faceCascade = cv.Load('haarcascade_frontalface_alt_tree.xml')
+
+    imgList=glob.glob(imagePattern)
+    if len(imgList)<=0:
+        print('No Images Found')
+        return
+
+    for img in imgList:
+        pil_im=Image.open(img)
+        pil = resizeImage(pil_im)
+        cv_im=pil2cvGrey(pil_im)
+        faces=DetectFace(cv_im,faceCascade)
+        if faces:
+            n=1
+            for face in faces:
+                croppedImage=imgCrop(pil_im, face[0],boxScale=boxScale)
+                fname,ext=os.path.splitext(img)
+                croppedImage.save("cropped/"+fname+'_crop'+str(n)+ext)
+                n+=1
+        else:
+            print('No faces found:')
+            print(img) 
+
+
+
+def resizeImage(image, size=(1024,768)):
+        print(image.size)
+        try :
+            image.thumbnail(size, Image.ANTIALIAS)
+        except IOError:
+            print "cannot reduce image for "
+        print(image.size)
+        return image   
+
+
+# Crop all jpegs in a folder. Note: the code uses glob which follows unix shell rules.
+# Use the boxScale to scale the cropping area. 1=opencv box, 2=2x the width and height
+faceCrop('Avinash.jpg',boxScale=1.5)
